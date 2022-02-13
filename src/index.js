cd074e72eb7e53c60b54b32768223b0462cc26e0
Yash <yashharale2003@gmail.com>
2022-02-13 17:18:46 +0530

    git init

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+app.use(mongoose)
 
 app.listen(port , () => {
     console.log('Server is up on port ' + port)
